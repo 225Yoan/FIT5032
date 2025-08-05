b469ce13097365a9c9b886dd97a86137a07121ac
Charli Ying <3256106367@qq.com>
2025-08-05 18:45:11 +1000

    Week 2

---
+++ yying-library/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import JSONLab from './components/JSON.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <JSONLab />
+  </main>
+</template>
+
+<style scoped>
+/* existing styles */
+</style>
