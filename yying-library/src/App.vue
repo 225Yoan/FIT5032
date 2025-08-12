cb6a0b84e9f32e23f8c3746ca6d4717fd01cc2ba
Charli Ying <3256106367@qq.com>
2025-08-12 22:40:13 +1000

    Updated yying-library of week3 studio

--- yying-library/src/App.vue
+++ yying-library/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import JSONLab from './components/JSON.vue'
+// import JSONLab from './components/JSON.vue'
+import Form from "./components/Form.vue";
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
-
-  <main>
+  </header> -->
+  <!-- <main>
     <JSONLab />
-  </main>
+  </main> -->
+  <Form />
 </template>
 
 <style scoped>
